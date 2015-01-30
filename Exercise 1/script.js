78d134587b357ba01cdee350330f3ba42182017a
siqister <siqi.zhu@gmail.com>
2015-01-30 11:51:54 -0500

    Added console.log command

--- Exercise 1/script.js
+++ Exercise 1/script.js
@@ -7,14 +7,27 @@ console.log("Start...");
 var me = {
         name:"Siqi Zhu",
         student:false,
-        class:"ARTG5330"
+        class:"ARTG5330",
+        id:"siqizhu",
+        age:"29",
+        school:{
+            name:"NEU",
+            fullName:"Northeastern University",
+            studentPop:30000
+        }
     };
 
 //Part 1: Log out a sentence in the form of:
 //"Siqi Zhu is an instructor in course ARTG5330, and has an ID# of ..."
-
-console.log();
+console.log(me.name + " is a student in course " + me.class + ", and has an ID of " + me.id );
 
 //Part 2: add a property on the fly, and log out the object
+me.age = +me.age;
+me.favoriteFood = "cookies";
 
 //Part 3: delete a property on the fly, and log out the object
+delete(me.student);
+
+console.log(me.school.name);
+console.log("End...");
+console.log();
