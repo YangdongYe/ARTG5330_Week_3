784bebba86c1d09e03c421399c7d202c6c447a5f
siqister <siqi.zhu@gmail.com>
2015-01-30 09:13:57 -0500

    First commit

---
+++ Exercise 1/script.js
@@ -0,0 +1,20 @@
+/**
+ * Week Class In-Class Exercise 1: Playing with Objects
+ */
+
+console.log("Start...");
+
+var me = {
+        name:"Siqi Zhu",
+        student:false,
+        class:"ARTG5330"
+    };
+
+//Part 1: Log out a sentence in the form of:
+//"Siqi Zhu is an instructor in course ARTG5330, and has an ID# of ..."
+
+console.log();
+
+//Part 2: add a property on the fly, and log out the object
+
+//Part 3: delete a property on the fly, and log out the object
